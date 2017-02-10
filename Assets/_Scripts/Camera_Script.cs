ce1ea18fac04b2220a19b61c9e143e7c7abddd06
20juanpls <s106020@rrhs-943-03.rrisd.local>
2017-02-10 14:35:10 -0600

    zoomin zoomout UI worked on

---
+++ Assets/_Scripts/Camera_Script.cs
@@ -0,0 +1,123 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Camera_Script : MonoBehaviour {
+	Camera cam;
+	public GameObject ZoomHud;
+
+	public int FieldOfView_LV,ZoomStep;
+
+	public float ZoomSpeed = 2.0f, TimeInterval = 1.0f;
+	public float ZoomD_0, ZoomD_1, ZoomD_2, ZoomD_3, ZoomD_4, ZoomD_5;
+
+	//public float[] DistArr = new float[5];
+	public ArrayList DistArr;
+
+	float FinalZoom, CurrentZoom, OrigZoom;
+
+	private bool ZoomIn, ZoomOut;
+	// Use this for initialization
+	void Start () {
+		if (DistArr == null) {
+			DistArr = new ArrayList();
+		}
+
+		ZoomHud = GameObject.Find("ZoomButtonsCanvas");
+		AssignZoomButtons ();
+			
+		cam = this.GetComponent<Camera> ();
+		OrigZoom = cam.orthographicSize;
+
+		FinalZoom = OrigZoom;
+		ZoomD_1 = OrigZoom;
+		ZoomStep = 1;
+		FieldOfView_LV = 1;
+
+		DistArr.Add (ZoomD_0);
+		DistArr.Add (ZoomD_1);
+		DistArr.Add (ZoomD_2);
+		DistArr.Add (ZoomD_3);
+		DistArr.Add (ZoomD_4);
+		DistArr.Add (ZoomD_5);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		CurrentZoom = cam.orthographicSize;
+
+		ZoomUpgrade ();
+
+		ZoomActivator ();
+		CamZoomSetter ();
+
+	}
+
+	void ZoomActivator(){
+		if (ZoomIn == true/*Input.GetKeyDown(KeyCode.O)*/ && ZoomStep > 0){
+			//Debug.Log("ZoomIn!");
+
+			ZoomSpeed = ((float)DistArr [ZoomStep] - (float)DistArr [ZoomStep - 1]) / TimeInterval;
+
+			ZoomStep--;
+			FinalZoom = (float)DistArr[ZoomStep];
+		}
+		if (ZoomOut == true/*Input.GetKeyDown (KeyCode.P)*/ && ZoomStep < FieldOfView_LV) {
+			//Debug.Log ("ZoomOut");
+
+			ZoomSpeed = ((float)DistArr [ZoomStep + 1] - (float)DistArr [ZoomStep]) / TimeInterval;
+
+			ZoomStep++;
+			FinalZoom = (float)DistArr [ZoomStep];
+
+		}
+		ZoomIn = false;
+		ZoomOut = false;
+	}
+
+	void CamZoomSetter(){
+		if (CurrentZoom != FinalZoom){
+			if (CurrentZoom < FinalZoom) {
+				CurrentZoom += (ZoomSpeed * Time.deltaTime);
+			}
+			if (CurrentZoom > FinalZoom) {
+				CurrentZoom -= (ZoomSpeed * Time.deltaTime);
+			}
+		}
+
+		cam.orthographicSize = CurrentZoom;
+	}
+
+	void ZoomUpgrade(){
+		if (Input.GetKeyDown (KeyCode.L) && FieldOfView_LV < DistArr.Count -1 ) {
+			FieldOfView_LV++;
+		}
+		if (Input.GetKeyDown (KeyCode.K) && FieldOfView_LV > 1) {
+			FieldOfView_LV--;
+		}
+
+		if (ZoomStep > FieldOfView_LV) {
+			ZoomStep = FieldOfView_LV;
+			FinalZoom = (float)DistArr [ZoomStep];
+		}
+	}
+
+	void AssignZoomButtons(){
+		Button ZoomIn = ZoomHud.transform.GetChild (0).GetComponent<Button> ();
+		Button ZoomOut = ZoomHud.transform.GetChild (1).GetComponent<Button> ();
+
+		ZoomIn.onClick.AddListener (delegate {
+			TheZoomIn();
+		});
+		ZoomOut.onClick.AddListener (delegate {
+			TheZoomOut();
+		});
+	}
+
+	public void TheZoomIn(){
+		ZoomIn = true;
+	}
+	public void TheZoomOut(){
+		ZoomOut = true;
+	}
+}
---
+++ Assets/_Scripts/ZoomLightScript.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class ZoomLightScript : MonoBehaviour {
+	GameObject cam;
+	GameObject thisZoomCanvas;
+	Camera_Script CamScript;
+
+	public GameObject LightContainer;
+	public GameObject TheLight;
+
+	public ArrayList LEDs;
+
+
+	// Use this for initialization
+	void Start () {
+		if (LEDs == null)
+			LEDs = new ArrayList ();
+
+		cam = GameObject.Find ("Main Camera");
+		CamScript = cam.GetComponent<Camera_Script> ();
+		thisZoomCanvas = this.gameObject;
+
+		AddLEDs();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void AddLEDs(){
+		for (int i = 0; i < CamScript.FieldOfView_LV; i++) {
+			GameObject _LED = (GameObject)Instantiate (LightContainer);
+			_LED.transform.SetParent(thisZoomCanvas.transform);
+			_LED.transform.localScale = new Vector3 (1.0f, 1.0f, 1.0f);
+			_LED.GetComponent<RectTransform>().anchoredPosition = new Vector3(88.0f + (i * 100), -60.0f, 0.0f);
+			LEDs.Add (_LED);
+		}
+	}
+}
