afd1a1978c9b73d25fd67d2d45de91c2d52c6473
BricenoNunez.Omar.s106020 <s106020@rrhs-943-15.rrisd.local>
2017-03-28 15:47:04 -0500

    guttenTAG

---
+++ Assets/InstructionBuildScript.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class InstructionBuildScript : MonoBehaviour {
+	public string sceneName;
+	Text PressI;
+	GameObject Instruct;
+	public bool InstructOnOff = false;
+
+
+
+	// Use this for initialization
+	void Start () {
+		PressI = GameObject.Find ("PressI").GetComponent<Text> ();
+		Instruct = GameObject.Find ("Instruct");
+		Instruct.SetActive (false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (InstructOnOff == false) {
+			if (Input.GetKeyDown (KeyCode.I)) {
+				Instruct.SetActive (true);
+				InstructOnOff = true;
+				PressI.text = "Type \"I\" to disable Instructions for this build";
+			}
+		} else if (InstructOnOff == true) {
+			if (Input.GetKeyDown (KeyCode.I)) {
+				Instruct.SetActive (false);
+				InstructOnOff = false;	
+				PressI.text = "Type \"I\" to enable Instructions for this build";
+			}
+		
+		}
+
+
+		if (Input.GetKeyDown (KeyCode.X)) {
+			SceneManager.LoadScene(sceneName);
+		}
+	}
+}
---
+++ Assets/thisscenecontroll.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class thisscenecontroll : MonoBehaviour {
+	public string sceneName;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.X)) {
+			SceneManager.LoadScene(sceneName);
+		}
+	}
+}
